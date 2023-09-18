648082ee7e795577940d4098b850f075887db5cc
ja88a <r0g3r@tuta.io>
2023-09-18 15:03:17 +0200

    style: :art: better code validation

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,14 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        "selector": "interface",
+        "format": ["PascalCase"],
+        "prefix": ["I"]
+      }
+    ],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
