9e603a7e09ebd7151d3534b71b5b2d8c0094fdef
ja88a <r0g3r@tuta.io>
2023-10-03 13:34:00 +0200

    fix: :bug: fix the indexing following last big refacting of account-ingestor

--- src/account-ingestor/data/index.ts
+++ src/account-ingestor/data/index.ts
@@ -1,3 +1,4 @@
 export * from './account-update.dto';
 export * from './class-validator';
 export * from './service.dto';
+export * from './rest-api.dto';
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export * from './app.module';
+export * as common from './common/index';
+export * as ingestor from './account-ingestor/index';
+export * as health from './health';
+export * as metrics from './metrics';
+export * as prometheus from './prometheus';
